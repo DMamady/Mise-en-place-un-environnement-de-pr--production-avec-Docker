031d1246d02c2937d763131704ff4fe2e4785e1e
DMamady <132494924+DMamady@users.noreply.github.com>
2024-12-07 21:05:17 +0100

    Update deploy_firewall.sh

--- scripts/deploy_firewall.sh
+++ scripts/deploy_firewall.sh
@@ -2,19 +2,9 @@
 
 # Construction de l'image Pare-feu
 docker build -t conteneurparefeuimage:v1.0.0 -f ../ContainerFirewallFile .
-echo "Création de l’image Docker du pare-feu..."
 
 # Lancement du conteneur Pare-feu
 docker run -d --cap-add=NET_ADMIN --name ContainerPareFeu --network bridge1 --ip 192.168.0.2 conteneurparefeuimage:v1.0.0 sleep infinity
-echo "Déploiement du conteneur Firewall..."
 
 # Connexion au deuxième réseau
 docker network connect bridge2 --ip 10.0.0.2 ContainerPareFeu
-echo "Connexion du conteneur Firewall à bridge2..."
-
-# Configuration interne du Pare-feu
-docker exec ContainerPareFeu bash -c "
-    echo 1 > /proc/sys/net/ipv4/ip_forward &&
-    iptables -t nat -A POSTROUTING -s 192.168.0.0/24 -d 10.0.0.0/24 -j MASQUERADE &&
-    iptables -t nat -A POSTROUTING -s 10.0.0.0/24 -d 192.168.0.0/24 -j MASQUERADE
-"
