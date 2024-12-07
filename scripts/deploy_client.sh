1affc041ff3e8b59b5fa311f2263b364aa24bfa1
DMamady <132494924+DMamady@users.noreply.github.com>
2024-12-07 21:05:43 +0100

    Update deploy_client.sh

--- scripts/deploy_client.sh
+++ scripts/deploy_client.sh
@@ -2,12 +2,9 @@
 
 # Construction de l'image Client
 docker build -t conteneurclientimage:v1.0.0 -f ../ContainerClientFile .
-echo "L'image du conteneur Client Créer avec succès"
 
 # Lancement du conteneur Client
 docker run -d --cap-add=NET_ADMIN --name ContainerClient --network bridge1 --ip 192.168.0.3 conteneurclientimage:v1.0.0 sleep infinity
-echo "Deploiement du Client container avec succès"
 
 # Configuration des routes statiques
 docker exec ContainerClient ip route add 10.0.0/24 via 192.168.0.2
-echo "Addition des route static vers le bridge2 avec le pare-feu"
